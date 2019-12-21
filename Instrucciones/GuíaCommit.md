cc7cd9a0e210b4f9fcad99d4a507d936b08008ff
Marco Perdomo <marcooperdomo@gmail.com>
2019-12-21 10:50:11 -0600

    Como mostrar HTML con las vistas parte 1

--- Instrucciones/GuíaCommit.md
+++ Instrucciones/GuíaCommit.md
@@ -1,7 +1,7 @@
 
 <style>body{background-color:black;} strong{color:crimson;} pre{background-color:red;}</style>
 <h1 align="center">Proyecto Curso Dominando Laravel con Laravel 5.6</h1>
-<h3><b>Commit -</b> <strong>Rutas con nombre</strong></h3>
+<h3><b>Commit -</b> <strong>Como mostrar HTML con las vistas parte 1</strong></h3>
 <hr>
 <ol>
   <!-- Instrucciones del commit -->
@@ -12,7 +12,5 @@
 
 <!-- Notas -->
 <h3><b>Notas:</b></h3>
-<ul>
-  <li>También llamadas Named Routes</li>
-</ul>
+
 <em>*Más información en el directorio routes/web.php</em>
--- routes/web.php
+++ routes/web.php
@@ -1,61 +1,16 @@
 <?php
 
 /* Notas:
-  *El 'echo' solo se está usando como ejemplo (no es la forma correcta en laravel).
-  *Suponiendo que el cliente nos pide cambiar la url de contactos por contactanos,
-   se debe cambiar manualmente cada ruta o usar search and replace del editor de código y es tedioso,
-   en este ejemplo hay 5 rutas que se tienen que cambiar ¿y si fueran 100 o 1000?, para eso son las rutas con nombre ya que
-   al llamar la ruta lo haces por el nombre de la ruta y no por la url.
+  *Es buena práctica darle nombre a las rutas.
+  *Para retornar una vista se usa la función view con el parámetro de la ruta de la vista.
+    *Ejemplo: return view( 'welcome' );
+  *Al retornar vistas desde la función view() de la ruta observa que no se debe poner la ruta completa ni la extensión,
+   ya que la función view() asume que las vistas están en resources/views y que su extensión es blade.php
 */
 
-// Ejemplo de ruta '/contactos'
-Route::get( 'contactos', function() {   
-  return "Sección de contactos";
-});
-
-// Ejemplo de ruta '/'
+// Ejemplo ruta '/'
 Route::get( '/', function() {
-  echo "<a href='/contactos'>Contactos 1</a><br>";
-  echo "<a href='/contactos'>Contactos 2</a><br>";
-  echo "<a href='/contactos'>Contactos 3</a><br>";
-  echo "<a href='/contactos'>Contactos 4</a><br>";
-  echo "<a href='/contactos'>Contactos 5</a><br>";
-});
+    
+  return view( 'welcome' );
 
-// Ejemplo de ruta '/contactanos'
-Route::get( 'contactanos', function() {
-  return "Sección de contactos";
-});
-
-// Ejemplo de ruta '/'
-Route::get( '/', function() {
-  echo "<a href='/contactanos'>Contactos 1</a><br>";
-  echo "<a href='/contactanos'>Contactos 2</a><br>";
-  echo "<a href='/contactanos'>Contactos 3</a><br>";
-  echo "<a href='/contactanos'>Contactos 4</a><br>";
-  echo "<a href='/contactanos'>Contactos 5</a><br>";
-});
-
-/* Notas:
-  *Las rutas con nombre permiten solo tener que cambiar la url en las rutas '/' aunque se tengan muchas rutas.
-    *Se usa el helper route('nombreRuta') y poder usar las rutas con nombre.
-  *Para asignarle un nombre a la ruta: Route::get('/')->name('nombreRuta')
-*/
-
-// Ejemplo de ruta con nombre '/contactame'
-Route::get( 'contactame', function() {    
-  return "Sección de contactos";
-})->name( 'contactos1' );
-
-Route::get( '/', function() {
-  echo "<a href='" . route('contactos1') . "'>Contactos 1</a><br>";
-  echo "<a href='" . route('contactos1') . "'>Contactos 2</a><br>";
-  echo "<a href='" . route('contactos1') . "'>Contactos 3</a><br>";
-  echo "<a href='" . route('contactos1') . "'>Contactos 4</a><br>";
-  echo "<a href='" . route('contactos1') . "'>Contactos 5</a><br>";
-});
-
-/* Notas:
-  *Retornar cadena de caracteres (string) en las funciones de las rutas no es lo correcto,
-   ya se verá más adelante en el curso como retornar vistas.
-*/
+})->name( 'home' );
