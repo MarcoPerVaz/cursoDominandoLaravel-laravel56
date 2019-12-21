685e60e9fff3898b2ff24a1de524cbc6cdcc5fdf
Marco Perdomo <marcooperdomo@gmail.com>
2019-12-21 10:04:16 -0600

    Rutas con nombre

--- Instrucciones/GuíaCommit.md
+++ Instrucciones/GuíaCommit.md
@@ -1,7 +1,7 @@
 
 <style>body{background-color:black;} strong{color:crimson;} pre{background-color:red;}</style>
 <h1 align="center">Proyecto Curso Dominando Laravel con Laravel 5.6</h1>
-<h3><b>Commit -</b> <strong>Qué son y cómo funcionan las rutas parte 2</strong></h3>
+<h3><b>Commit -</b> <strong>Rutas con nombre</strong></h3>
 <hr>
 <ol>
   <!-- Instrucciones del commit -->
@@ -13,7 +13,6 @@
 <!-- Notas -->
 <h3><b>Notas:</b></h3>
 <ul>
-  <li>Existen 2 principales archivos de rutas: rutas web y rutas api</li>
-  <li>Las rutas son las url de la aplicación</li>
+  <li>También llamadas Named Routes</li>
 </ul>
 <em>*Más información en el directorio routes/web.php</em>
--- routes/web.php
+++ routes/web.php
@@ -1,24 +1,61 @@
 <?php
-/* Notas:
-    *Los parámetros en las rutas deben recibirse a tráves de los parámetros de 
-     la función (En este caso en específico).
-*/
-
-// Ejemplo ruta 'saludo/{parametro}'con parámetro obligatorio
-Route::get( 'saludo/{nombre}', function( $nombre ) {
-    return "saludos " . $nombre;
-});
-// En el navegador: https://cursodominandolaravel-laravel56.it/saludo/marco
-
 
 /* Notas:
-    *Los parámetros opcionales en las rutas llevan un '?' y los parámetros opcionales dentro de la función 
-     deben ser incializados ejemplo: $nombre = "Invitado".
+  *El 'echo' solo se está usando como ejemplo (no es la forma correcta en laravel).
+  *Suponiendo que el cliente nos pide cambiar la url de contactos por contactanos,
+   se debe cambiar manualmente cada ruta o usar search and replace del editor de código y es tedioso,
+   en este ejemplo hay 5 rutas que se tienen que cambiar ¿y si fueran 100 o 1000?, para eso son las rutas con nombre ya que
+   al llamar la ruta lo haces por el nombre de la ruta y no por la url.
 */
 
-// Ejemplo de ruta 'saludo1/{parametro?}' con parámetro opcional
-Route::get( 'saludo1/{nombre?}', function( $nombre = "Invitado" ) {
-    return "saludos " . $nombre;
+// Ejemplo de ruta '/contactos'
+Route::get( 'contactos', function() {   
+  return "Sección de contactos";
 });
-// En el navegador: https://cursodominandolaravel-laravel56.it/saludo1/
-// En el navegador: https://cursodominandolaravel-laravel56.it/saludo1/Marco
+
+// Ejemplo de ruta '/'
+Route::get( '/', function() {
+  echo "<a href='/contactos'>Contactos 1</a><br>";
+  echo "<a href='/contactos'>Contactos 2</a><br>";
+  echo "<a href='/contactos'>Contactos 3</a><br>";
+  echo "<a href='/contactos'>Contactos 4</a><br>";
+  echo "<a href='/contactos'>Contactos 5</a><br>";
+});
+
+// Ejemplo de ruta '/contactanos'
+Route::get( 'contactanos', function() {
+  return "Sección de contactos";
+});
+
+// Ejemplo de ruta '/'
+Route::get( '/', function() {
+  echo "<a href='/contactanos'>Contactos 1</a><br>";
+  echo "<a href='/contactanos'>Contactos 2</a><br>";
+  echo "<a href='/contactanos'>Contactos 3</a><br>";
+  echo "<a href='/contactanos'>Contactos 4</a><br>";
+  echo "<a href='/contactanos'>Contactos 5</a><br>";
+});
+
+/* Notas:
+  *Las rutas con nombre permiten solo tener que cambiar la url en las rutas '/' aunque se tengan muchas rutas.
+    *Se usa el helper route('nombreRuta') y poder usar las rutas con nombre.
+  *Para asignarle un nombre a la ruta: Route::get('/')->name('nombreRuta')
+*/
+
+// Ejemplo de ruta con nombre '/contactame'
+Route::get( 'contactame', function() {    
+  return "Sección de contactos";
+})->name( 'contactos1' );
+
+Route::get( '/', function() {
+  echo "<a href='" . route('contactos1') . "'>Contactos 1</a><br>";
+  echo "<a href='" . route('contactos1') . "'>Contactos 2</a><br>";
+  echo "<a href='" . route('contactos1') . "'>Contactos 3</a><br>";
+  echo "<a href='" . route('contactos1') . "'>Contactos 4</a><br>";
+  echo "<a href='" . route('contactos1') . "'>Contactos 5</a><br>";
+});
+
+/* Notas:
+  *Retornar cadena de caracteres (string) en las funciones de las rutas no es lo correcto,
+   ya se verá más adelante en el curso como retornar vistas.
+*/
